1a5d21977b2b55cb2cd8eeb1f92c967381426e88
ukirdeomkar <ukirdeomkar4@gmail.com>
2023-09-23 20:25:38 +0530

    react-router and navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <div> This is NoteKeeper App </div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+    </BrowserRouter>
     </>
   );
 }
